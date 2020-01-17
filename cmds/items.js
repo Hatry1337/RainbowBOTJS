c152da0e2ffce6796f53e385404dab54c6bb158c
Hatrychannel@gmail.com <Hatrychannel@gmail.com>
2020-01-17 23:45:07 +0300

    add some features to !items cmd

--- cmds/items.js
+++ cmds/items.js
@@ -14,16 +14,27 @@ function items(message, Discord, db, client, gu, uu) {
             message.channel.send("Данный пользователь не зарегистрирован!");
             return;
         }
+        var bm1_rate = 1.2;
+        var bm2_rate = 2.5;
+        var bmr_rate = 5.6;
+        var bmdc_rate = 27.8;
+        var ss_rate = 2.8;
+
+        var mining_speed = (bm1_rate * user.bitminer1) + (bm2_rate * user.bitminer2) + (bmr_rate * user.bitminer_rack) + (bmdc_rate * user.bitm_dc) * 60 * 60
+
         emb = new Discord.RichEmbed()
             .setTitle(`Предметы игрока ${user.user}:`)
             .setColor(0x228b22)
-            .setDescription(
-                `Количество Поинтов: ${user.user_points}
-                Bitminer S1: ${user.bitminer1}
-                Bitminer S2: ${user.bitminer2}
-                Bitminer Rack: ${user.bitminer_rack}
-                Датацентр с майнерами: ${user.bitm_dc}
-                Солнечная Электростанция: ${user.solar_station}`);
+            .setDescription(`
+Количество Поинтов: ${user.user_points}
+Bitminer S1: ${user.bitminer1}
+Bitminer S2: ${user.bitminer2}
+Bitminer Rack: ${user.bitminer_rack}
+Датацентр с майнерами: ${user.bitm_dc}
+Солнечная Электростанция: ${user.solar_station}
+
+Общая скорость майнинга: ${mining_speed} Поинтов в час.               
+`);
         message.channel.send(embed = emb);
         return;
     });
