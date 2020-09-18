0ebc691e912460520c19c49b9fae7ee896ea9789
Thomas <Hatrychannel@gmail.com>
2020-09-18 21:41:22 +0300

    osuinfo fix

--- cmds/osuinfo.js
+++ cmds/osuinfo.js
@@ -69,42 +69,47 @@ class OsuInfo {
                     form: {k: 'e5d6051cbf2455fd3f2c1240208aa4812594c6f0', u: osuUser, m: gameMode, limit: 1}
                 }, function (err, httpResponse, score_b) {
                     var score = JSON.parse(score_b)[0];
-                    othis.Request.post({
-                        url: 'https://osu.ppy.sh/api/get_beatmaps',
-                        form: {k: 'e5d6051cbf2455fd3f2c1240208aa4812594c6f0', b: score.beatmap_id, limit: 1}
-                    }, function (err, httpResponse, map_b) {
-                        var map = JSON.parse(map_b)[0];
-                        var emb = new othis.Discord.MessageEmbed()
-                            .setColor(0xff669c)
-                            .setTitle(`${othis.Client.emojis.cache.find(emoji => emoji.name === `osu_md${gameMode}`)} :flag_${data.country.toLowerCase()}: ${data.username}'s Profile`)
-                            .setThumbnail(`https://a.ppy.sh/${data.user_id}`)
-                            .addFields([
-                                {name: "Рейтинг в мире", value: `#${data.pp_rank}`, inline: true},
-                                {name: "Рейтинг в стране", value: `#${data.pp_country_rank}`, inline: true},
-                                {name: "Профиль", value: `[${data.username}](https://osu.ppy.sh/users/${data.user_id})`, inline: true},
-                                {name: "PP", value: `${parseInt(data.pp_raw)}`, inline: true},
-                                {name: "Точность", value: `${parseFloat(data.accuracy).toFixed(2)}%`, inline: true},
-                                {name: "Игр сыграно", value: `${data.playcount}`, inline: true},
-                                {name: "Рейтинговые очки", value: `${data.ranked_score}`, inline: true},
-                                {name: "Всего очков", value: `${data.total_score}`, inline: true},
-                                {name: "Уровень", value: `${parseInt(data.level)}`, inline: true},
-                                {
-                                    name: "Времени в игре",
-                                    value: `${othis.timeConversionOsu(data.total_seconds_played * 1000)}`,
-                                    inline: true
-                                },
-                                {name: "Дата регистрации", value: `${data.join_date}`, inline: true},
-                            ]);
-                        if(map){
-                            emb.addFields([{name: "Best Score", value: `[${Number((parseFloat(map.difficultyrating)).toFixed(2))}★] ${othis.Client.emojis.cache.find(emoji => emoji.name === `rank_${score.rank}`)} [**${map.title}** от **${map.artist}**](https://osu.ppy.sh/b/${map.beatmap_id}) ${Number(parseFloat(score.pp)).toFixed(0)}pp`, inline: false},])
-                        }
-                        message.channel.send(emb);
-                        othis.Database.writeLog('osuinfo', message.author.id, message.guild.name,
-                            JSON.stringify({
-                                Message: `User '${message.author.tag}' watched osu! profile of user '${data.username}'.`
-                        }));
+                    if(score){
+                        othis.Request.post({
+                            url: 'https://osu.ppy.sh/api/get_beatmaps',
+                            form: {k: 'e5d6051cbf2455fd3f2c1240208aa4812594c6f0', b: score.beatmap_id, limit: 1}
+                        }, function (err, httpResponse, map_b) {
+                            var map = JSON.parse(map_b)[0];
+                            var emb = new othis.Discord.MessageEmbed()
+                                .setColor(0xff669c)
+                                .setTitle(`${othis.Client.emojis.cache.find(emoji => emoji.name === `osu_md${gameMode}`)} :flag_${data.country.toLowerCase()}: ${data.username}'s Profile`)
+                                .setThumbnail(`https://a.ppy.sh/${data.user_id}`)
+                                .addFields([
+                                    {name: "Рейтинг в мире", value: `#${data.pp_rank}`, inline: true},
+                                    {name: "Рейтинг в стране", value: `#${data.pp_country_rank}`, inline: true},
+                                    {name: "Профиль", value: `[${data.username}](https://osu.ppy.sh/users/${data.user_id})`, inline: true},
+                                    {name: "PP", value: `${parseInt(data.pp_raw)}`, inline: true},
+                                    {name: "Точность", value: `${parseFloat(data.accuracy).toFixed(2)}%`, inline: true},
+                                    {name: "Игр сыграно", value: `${data.playcount}`, inline: true},
+                                    {name: "Рейтинговые очки", value: `${data.ranked_score}`, inline: true},
+                                    {name: "Всего очков", value: `${data.total_score}`, inline: true},
+                                    {name: "Уровень", value: `${parseInt(data.level)}`, inline: true},
+                                    {
+                                        name: "Времени в игре",
+                                        value: `${othis.timeConversionOsu(data.total_seconds_played * 1000)}`,
+                                        inline: true
+                                    },
+                                    {name: "Дата регистрации", value: `${data.join_date}`, inline: true},
+                                ]);
+                            if(map){
+                                emb.addFields([{name: "Best Score", value: `[${Number((parseFloat(map.difficultyrating)).toFixed(2))}★] ${othis.Client.emojis.cache.find(emoji => emoji.name === `rank_${score.rank}`)} [**${map.title}** от **${map.artist}**](https://osu.ppy.sh/b/${map.beatmap_id}) ${Number(parseFloat(score.pp)).toFixed(0)}pp`, inline: false},])
+                            }
+                            message.channel.send(emb);
+                            othis.Database.writeLog('osuinfo', message.author.id, message.guild.name,
+                                JSON.stringify({
+                                    Message: `User '${message.author.tag}' watched osu! profile of user '${data.username}'.`
+                                }));
+                            return;
+                        });
+                    }else {
+                        message.channel.send("Такой пользователь не найден!");
                         return;
-                    });
+                    }
                 });
             });
 
